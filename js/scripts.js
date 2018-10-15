fc179a4af32cb047f31af353492a314a157bb7cf
James Cho and Kenny Wolfenberger <kenny@>
2018-10-15 11:43:47 -0700

    update with additional calculator functions

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,42 @@ var add = function(number1, number2) {
     var number2 = parseInt(prompt("Enter another number: "));
     
     alert(add(number1, number2));
+
+var subtract = function(number1, number2) {
+    return number1 - number2;
+    }
+
+    var number1 = parseInt(prompt("Enter a number: "));
+    var number2 = parseInt(prompt("Enter another number: "));
+    alert(subtract(number1, number2));
+
+var multiply = function(number1, number2) {
+        return number1 * number2;
+        }
+    
+        var number1 = parseInt(prompt("Enter a number: "));
+        var number2 = parseInt(prompt("Enter another number: "));
+        alert(multiply(number1, number2));
+
+var divide = function(number1, number2) {
+            return number1 / number2;
+            }
+        
+        var number1 = parseInt(prompt("Enter a number: "));
+        var number2 = parseInt(prompt("Enter another number: "));
+        alert(divide(number1, number2));
+
+
+        
+        
+        /* Cooking Conversion
+        
+        var cookingConversion = function(cup){
+            var tbsp = parseInt(prompt("How many cups?"));
+          var result = tbsp * 16;
+          alert(result);
+        
+        }
+        
+        
+        cookingConversion(); */
